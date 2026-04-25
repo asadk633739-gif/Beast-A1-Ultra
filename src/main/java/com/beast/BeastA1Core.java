37db624c7016bf27cea024ac52b10955eb93e5ce
asadk633739-gif <asadk633739@gmail.com>
2026-04-25 17:59:06 +0530

    Update BeastA1Core.java

--- src/main/java/com/beast/BeastA1Core.java
+++ src/main/java/com/beast/BeastA1Core.java
@@ -4,7 +4,8 @@ import net.fabricmc.api.ModInitializer;
 public class BeastA1Core implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Ye code Holy Renderer ke liye backend optimize karega
-        System.out.println("Beast Mod V1.0 - Ultra Performance Active!");
+        // CPU ko instruction ki game priority badhao
+        Thread.currentThread().setPriority(10);
+        System.out.println("Beast Mode: High Priority FPS Enabled");
     }
 }
