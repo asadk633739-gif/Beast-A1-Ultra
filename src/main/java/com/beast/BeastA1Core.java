c3016eb26b0852679d477dbc4f30bb4d6f59ea0d
asadk633739-gif <asadk633739@gmail.com>
2026-04-25 17:14:06 +0530

    Update BeastA1Core.java

--- src/main/java/com/beast/BeastA1Core.java
+++ src/main/java/com/beast/BeastA1Core.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 public class BeastA1Core implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("Beast Mod Active!");
+        // Ye code Holy Renderer ke liye backend optimize karega
+        System.out.println("Beast Mod V1.0 - Ultra Performance Active!");
     }
 }
