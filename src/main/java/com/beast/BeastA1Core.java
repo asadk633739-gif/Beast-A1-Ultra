f8494da3ed6a27a3ca56a443b121d7c39cc7ea00
asadk633739-gif <asadk633739@gmail.com>
2026-04-25 17:12:18 +0530

    Update BeastA1Core.java

--- src/main/java/com/beast/BeastA1Core.java
+++ src/main/java/com/beast/BeastA1Core.java
@@ -1,17 +1,9 @@
 package com.beast;
-
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.MinecraftClient;
 
 public class BeastA1Core implements ModInitializer {
     @Override
     public void onInitialize() {
-        System.out.println("Beast Mod: Optimized for Infinix Note 30 5G & Holy Renderer");
-        
-        // Speed up memory cleaning
-        Runtime.getRuntime().gc();
-        
-        // Setup high priority for game tasks
-        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Beast Mod Active!");
     }
 }
