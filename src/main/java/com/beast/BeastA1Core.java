4cd394e73a52152a38c724127eaa6efc562a3c61
asadk633739-gif <asadk633739@gmail.com>
2026-04-25 18:21:25 +0530

    Update BeastA1Core.java

--- src/main/java/com/beast/BeastA1Core.java
+++ src/main/java/com/beast/BeastA1Core.java
@@ -1,11 +1,10 @@
 package com.beast;
+
 import net.fabricmc.api.ModInitializer;
 
 public class BeastA1Core implements ModInitializer {
     @Override
     public void onInitialize() {
-        // CPU ko instruction ki game priority badhao
-        Thread.currentThread().setPriority(10);
-        System.out.println("Beast Mode: High Priority FPS Enabled");
+        System.out.println("Beast Mod A1 Initialized Successfully!");
     }
 }
